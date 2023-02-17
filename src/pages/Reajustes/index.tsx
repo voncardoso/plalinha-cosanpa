2ad804082e67ff2672988f6bcf31450a1daf75d7
voncardoso <vonharrison26@gmail.com>
2023-02-17 15:28:32 -0300

    ajuste na tabela

--- src/pages/Reajustes/index.tsx
+++ src/pages/Reajustes/index.tsx
@@ -1,98 +1,170 @@
-import { ContainerRegister, TableRegister } from "./style";
-import relatorioJson from "../../objetos/relatorio01.json"
 import { useState } from "react";
-import LogoCosampa from "../../assets/logo-cosanpa.png"
-import LogoEncibra from "../../assets/logo-encibra.png"
+import { priceFormatter } from "../../utils/formatter";
+import {
+  ContainerRegister,
+  TableRegister,
+  TableRegisterTotais,
+  TrTotais,
+} from "./style";
+import LogoCosampa from "../../assets/logo-cosanpa.png";
+import LogoEncibra from "../../assets/logo-encibra.png";
 
-export function Reajustes(){
-    const [teste, setTeste] = useState([])
+import relatorioJson from "../../objetos/relatorio01.json";
 
+export function Reajustes() {
+  const [teste, setTeste] = useState([]);
 
-    
-    console.log(teste)
-    function SplitArraySixty(){
-        var resultado: any = [];
-        var grupo = 0; 
+  let countItem = 0;
 
+  const total = relatorioJson.reduce(
+    (acc, value) => {
+      acc.totalContrato += value.contract_value;
+      acc.totalAditivo += value.additive_value;
+      acc.totalRequilibrio += value.rebalancing;
+      acc.totalReajuste += value.readjustment;
 
-        if (relatorioJson) {
-            for (let i = 0; i < relatorioJson.length; i++) {
-              if (resultado[grupo] === undefined) {
-                resultado[grupo] = [];
-              }
-      
-              resultado[grupo].push(relatorioJson[i]);
-      
-              if ((i + 1) % 10 === 0) {
-                grupo = grupo + 1;
-              }
-            }
-          }
-          let totalPaginasPhoto = relatorioJson.length / 4;
-          console.log(resultado)
-          return resultado;
+      acc.total2 +=
+        value.contract_value + value.additive_value + value.rebalancing;
+
+      acc.totalContratado += value.contract_value + value.additive_value;
+      acc.totalRerajusteProjetado += value.project_value;
+
+      acc.totalCusto +=
+        value.contract_value + value.additive_value + value.rebalancing;
+      +value.readjustment + value.project_value;
+
+      acc.totalMedido += value.payment;
+      return acc;
+    },
+    {
+      totalContrato: 0,
+      totalAditivo: 0,
+      totalRequilibrio: 0,
+      totalReajuste: 0,
+      total2: 0,
+      totalContratado: 0,
+      totalRerajusteProjetado: 0,
+      totalCusto: 0,
+      totalMedido: 0,
     }
-    
+  );
 
-    return(
-        <ContainerRegister>
-        <header>
-            <img  src={LogoEncibra} alt="" />
-            <h1>Gestão De Reajuste</h1>
-            <img src={LogoCosampa} alt="" />
-        </header>
-        <div>
-        {SplitArraySixty().map((item1: any) =>{
-            return(
-                <TableRegister>
-                <thead>
-                        <th className="fistTd"></th>
-                        <th className="contract_id">Nº</th>
-                        <th>Objeto</th>
-                        <th>Contrato</th>
-                        <th>Aditivo</th>
-                        <th>Total Contratado</th>
-                        <th>Reequilibrio</th>
-                        <th>Total II</th>
-                        <th>Reaguste Pago</th>
-                        <th>Reajuste Projetado</th>
-                        <th>Custo Total</th>
-                        <th>Meidido</th>
-                        <th>Saldo a medir</th>
-                </thead>
+  function SplitArraySixty() {
+    var resultado: any = [];
+    var grupo = 0;
 
-                <tbody>
-                    {item1.map((item: any, index: number) =>{
-                       return(
-                                <>
+    if (relatorioJson) {
+      for (let i = 0; i < relatorioJson.length; i++) {
+        if (resultado[grupo] === undefined) {
+          resultado[grupo] = [];
+        }
 
-                                <tr>
-                                    <td className="fistTd">{index}</td>
-                                    <td className="contract_id">{item.contract_id}</td>
-                                    <td className="ObjetoTd">
-                                       {item.contract_object}
-                                    </td>
-                                    <td>{561651651}</td>
-                                    <td>{item.contract_value}</td>
-                                    <td>{item.additive_value}</td>
-                                    <td>{15156165}</td>
-                                    <td>0561560165651</td>
-                                    <td>{365165165165}</td>
-                                    <td>{item.readjustment}</td>
-                                    <td>{554464646846464}</td>
-                                    <td>{1561651}</td>
-                                    <td>{item.payment}</td>
-                                </tr>
-                            </>
-                       )
-                    })}
-                </tbody>
-                </TableRegister> 
-                )
-        })}
-        </div>
-        
-        </ContainerRegister> 
+        resultado[grupo].push(relatorioJson[i]);
 
-    )
+        if ((i + 1) % 10 === 0) {
+          grupo = grupo + 1;
+        }
+      }
+    }
+    let totalPaginasPhoto = relatorioJson.length / 4;
+    console.log(resultado);
+    return resultado;
+  }
+
+  return (
+    <ContainerRegister>
+      <header>
+        <img src={LogoEncibra} alt="" />
+        <h1>Gestão De Reajuste</h1>
+        <img src={LogoCosampa} alt="" />
+      </header>
+      <div>
+        <TableRegister>
+          <thead>
+            <th className="fistTd"></th>
+            <th className="contract_id">Nº</th>
+            <th>Objeto</th>
+            <th>Contrato</th>
+            <th>Aditivo</th>
+            <th>Total Contratado</th>
+            <th>Reequilibrio</th>
+            <th>Total II</th>
+            <th>Reaguste Pago</th>
+            <th>Reajuste Projetado</th>
+            <th>Custo Total</th>
+            <th>Meidido</th>
+            <th>Saldo a medir</th>
+          </thead>
+          {SplitArraySixty().map((item1: any) => {
+            return (
+              <tbody>
+                {item1.map((item: any, index: number) => {
+                  let contratado = item.contract_value + item.additive_value;
+                  countItem++;
+                  return (
+                    <>
+                      <tr>
+                        <td className="fistTd">{countItem}</td>
+                        <td className="contract_id">{item.contract_id}</td>
+                        <td className="ObjetoTd">{item.contract_object}</td>
+                        <td>{priceFormatter.format(item.contract_value)}</td>
+                        <td>{priceFormatter.format(item.additive_value)}</td>
+                        <td>
+                          {priceFormatter.format(
+                            item.contract_value + item.additive_value
+                          )}
+                        </td>
+                        <td>{priceFormatter.format(item.rebalancing)}</td>
+                        <td>
+                          {priceFormatter.format(
+                            item.contract_value +
+                              item.additive_value +
+                              item.rebalancing
+                          )}
+                        </td>
+                        <td style={{ color: "#F26B66" }}>
+                          {priceFormatter.format(item.readjustment)}
+                        </td>
+                        <td style={{ color: "#64C1F5" }}>
+                          {priceFormatter.format(item.project_value)}
+                        </td>
+                        <td>
+                          {priceFormatter.format(
+                            item.contract_value +
+                              item.additive_value +
+                              item.rebalancing +
+                              item.readjustment +
+                              item.project_value
+                          )}
+                        </td>
+                        <td>{priceFormatter.format(item.payment)}</td>
+                        <td>
+                          {priceFormatter.format(contratado - item.payment)}
+                        </td>
+                      </tr>
+                    </>
+                  );
+                })}
+              </tbody>
+            );
+          })}
+          <TrTotais>
+            <td colSpan={3}>Total</td>
+            <td>{priceFormatter.format(total.totalContrato)}</td>
+            <td>{priceFormatter.format(total.totalAditivo)}</td>
+            <td>{priceFormatter.format(total.totalContratado)}</td>
+            <td>{priceFormatter.format(total.totalRequilibrio)}</td>
+            <td>{priceFormatter.format(total.total2)}</td>
+            <td>{priceFormatter.format(total.totalReajuste)}</td>
+            <td>{priceFormatter.format(total.totalRerajusteProjetado)}</td>
+            <td>{priceFormatter.format(total.totalCusto)}</td>
+            <td>{priceFormatter.format(total.totalMedido)}</td>
+            <td>
+              {priceFormatter.format(total.totalContratado - total.totalMedido)}
+            </td>
+          </TrTotais>
+        </TableRegister>
+      </div>
+    </ContainerRegister>
+  );
 }
--- src/pages/Reajustes/style.ts
+++ src/pages/Reajustes/style.ts
@@ -1,100 +1,130 @@
 import styled from "styled-components";
 
 export const ContainerRegister = styled.main`
-    width: 1180px;
+  width: 1180px;
+  margin: 0 auto;
+  font-size: 10px;
+  padding: 0px 10px 0px 20px;
+  header {
+    display: flex;
+    width: 1100px;
+    justify-content: space-between;
+    align-items: center;
     margin: 0 auto;
-    font-size: 10px;
-    padding: 0px 10px 0px 20px;
-    height: 100vh;
-    
+    padding: 10px;
 
-    header{
-        display: flex;
-        width: 1100px;
-        justify-content: space-between;
-        align-items: center;
-        margin: 0 auto;
-        padding: 10px;
-
-        h1{
-            font-size: 1rem;
-        }
-        img{
-            width: 40px;
-        }
-
-        .logoEncibra{
-            width: 200px;
-        }
+    h1 {
+      font-size: 1rem;
     }
-`
+    img {
+      width: 40px;
+    }
 
+    .logoEncibra {
+      width: 200px;
+    }
+  }
+`;
 
 export const TableRegister = styled.table`
+  border-collapse: collapse;
+  border: 1px solid black;
+  text-align: center;
+  margin: 0 auto;
+  position: relative;
+  top: 10px;
+  thead {
+    background: ${(props) => props.theme["green"]};
+    border: 1px solid black;
+    th {
+      min-width: 90px;
+      border: 1px solid black;
+      padding: 5px;
+      color: ${(props) => props.theme["whithe"]};
+      font-weight: 400;
+    }
+  }
+
+  tbody {
+    width: 100%;
+    border: 1px solid black;
+    page-break-inside: avoid;
+    page-break-after: auto;
+    tr {
+      background: ${(props) => props.theme["whithe"]};
+      border-bottom: 1px solid red;
+      td {
+        border: 1px solid black;
+        padding: 5px;
+        min-width: 94px;
+        max-width: 94px;
+        height: 72px;
+        font-size: 8px;
+      }
+    }
+  }
+
+  & + table {
+    position: relative;
+    margin-top: 30px;
+    margin-bottom: 60px;
+    top: 30px;
+  }
+
+  &::last-child {
+    color: red;
+  }
+
+  .totais {
+  }
+
+  .ObjetoTd {
+    width: 100px;
+    padding: 2px;
+  }
+
+  .fistTd {
+    min-width: 20px;
+    max-width: 40px;
+    font-weight: bold;
+  }
+
+  .contract_id {
+    min-width: 40px;
+  }
+
+  .tdHeigth {
+    height: 700px;
+    margin-bottom: 20px;
+  }
+`;
+
+export const TableRegisterTotais = styled.div`
+  width: 100%;
+  margin-bottom: 60px;
+  table {
+    width: 1100px;
     padding: 0px 0px;
     border-collapse: collapse;
     border: 1px solid black;
     text-align: center;
     margin: 0 auto;
-    thead{
-        margin-top: 10px;
-        background: ${(props) => props.theme["green"]};
-        border: 1px solid black;
-        th{
-       
-            min-width: 90px;
-            border: 1px solid black;
-            padding: 5px;
-            color: ${(props) => props.theme["whithe"]};
-            font-weight: 400;
-        }
+
+    .fistTd {
+      max-width: 20px;
+      font-weight: bold;
     }
+  }
+`;
 
-    tbody{
-        width: 100%;
-        margin-bottom: 60px;
-        border: 1px solid black;
-            tr{
-                background: ${(props) => props.theme["whithe"]};
-                border-bottom: 1px solid red;
-            td{
-                border: 1px solid black;
-                padding: 5px;
-                min-width: 94px;
-                max-width: 94px;
-                height: 72px;
-                font-size: 8px;
-
-            }
-
-        }   
-
-    }
-
-
-     & + table{
-        position: relative;
-        margin-top: 30px;
-        margin-bottom: 60px;
-        top: 30px;
-
-    }
-
-    .totais{
-    }
-
-    .ObjetoTd{
-        width: 100px;
-        padding: 2px;
-    }
-
-    .fistTd{
-            min-width: 20px;
-            max-width: 40px;
-            font-weight: bold;
-    }
-
-    .contract_id{
-        min-width: 40px;
-    }
-`
+export const TrTotais = styled.tr`
+  td {
+    border: 1px solid black;
+    background: ${(props) => props.theme["green-200"]};
+    color: ${(props) => props.theme["whithe"]};
+    font-size: 8px;
+    padding: 5px;
+    font-weight: bold;
+    letter-spacing: 0.2px;
+  }
+`;
